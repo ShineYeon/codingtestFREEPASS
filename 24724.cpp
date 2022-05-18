5fa491510f49a15e06e2e32d8fcea3e26e869a9e
ShineYeon <chlgmldus2@naver.com>
2022-05-18 15:37:00 +0900

    0518_24724

---
+++ 24724.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 최희연 on 2022/05/18.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    for(int i=1; i<=T; i++){
+        cout<<"Material Management "<<i<<"\n";
+        int N, A, B;
+        cin>>N;
+        cin>>A>>B;
+        while(N--){
+            int u, v;
+            cin>>u>>v;
+        }
+        cout<<"Classification ---- End!"<<"\n";
+
+
+    }
+}
---
+++ 2933.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 최희연 on 2022/05/18.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
